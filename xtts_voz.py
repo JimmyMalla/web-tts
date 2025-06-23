92e6184e291d7314371e148f07fd7cba7c3594a9
Jimmy Heriberto Malla Torres <55264235+JimmyMalla@users.noreply.github.com>
2025-06-23 10:00:16 -0500

    Update xtts_voz.py

--- xtts_voz.py
+++ xtts_voz.py
@@ -1,15 +1,10 @@
 from TTS.api import TTS
 
-# Carga el modelo XTTS v2
 tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
 
-# Texto que quieres transformar en voz
-texto = "El lobo no necesita demostrar su fuerza. Su silencio ya impone respeto."
-
-# Genera la voz y guarda el audio
 tts.tts_to_file(
-    text=texto,
-    file_path="static/audios/lobo_xtts.wav",
-    speaker_wav="samples/xtts_voz.wav",  # Ruta a la muestra de voz
-    language="es"
+    text="Hola, soy la voz del lobo sabio.",
+    speaker_wav="samples/voz_sabio.wav",
+    language="es",
+    file_path="voz_prueba.wav"
 )
